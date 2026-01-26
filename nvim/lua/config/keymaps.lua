1b6d4551fe34d496afc34a12ff00081bbb5b674b
Роман Лапин <r.lapin@prosoftsystems.ru>
2026-01-27 01:52:19 +0300

    Enhance work with lines

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -20,3 +20,16 @@ vim.keymap.set("i", "<S-Left>", "<C-o>b", { desc = "Scroll left in insert mode" 
 
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
+
+vim.keymap.set("n", "<A-Up>",   ":m .-2<CR>==", { desc = "Move line up" })
+vim.keymap.set("n", "<A-Down>", ":m .+1<CR>==", { desc = "Move line down" })
+
+vim.keymap.set("v", "<A-Up>",   ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+
+vim.keymap.set("i", "<A-Up>",   "<Esc>:m .-2<CR>==gi", { desc = "Move line up" })
+vim.keymap.set("i", "<A-Down>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down" })
+
+vim.keymap.set("n", "<Leader>o", "o<Esc>", { desc = "Empty line below" })
+vim.keymap.set("n", "<Leader>O", "O<Esc>", { desc = "Empty line above" })
+
