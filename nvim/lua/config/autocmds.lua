2408575fa3dcea5faab187adadd6bc84f5c6e48b
Роман Лапин <r.lapin@prosoftsystems.ru>
2026-03-27 14:31:56 +0300

    enh(git): replaced vim-fugitive with neogit

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,21 +1,7 @@
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "fugitive",
-	callback = function()
-		vim.opt_local.buflisted = false
-	end,
-})
-
 vim.api.nvim_create_autocmd("TermOpen", {
 	pattern = "*",
 	callback = function()
 		vim.opt_local.spell = false
-	end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "diff" },
-	callback = function()
-		vim.b.lsp_disable = true
 	end,
 })
 
