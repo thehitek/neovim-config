3c9829efbae85e065ef0c9dac12c64e178ba5453
Роман Лапин <r.lapin@prosoftsystems.ru>
2026-06-16 09:28:45 +0300

    added debugging tools and some enchancements

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -19,3 +19,11 @@ vim.api.nvim_create_autocmd("FileType", {
 		end
 	end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "markdown" },
+	callback = function()
+		vim.opt_local.linebreak = false
+		vim.opt_local.wrap = false
+	end,
+})
